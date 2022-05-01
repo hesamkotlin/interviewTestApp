22a8a4e4e0d076dc26ff5b42baa52da15c93399e
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-05-01 19:17:46 +0430

    add: Binding adapter for loading state

---
+++ app/src/main/java/com/example/interviewtestapp/ui/util/BindingAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.interviewtestapp.ui.util
+
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+
+
+@BindingAdapter("visibleGone")
+fun visibleOrGone(view: View, visible: Boolean?){
+    visible ?: return
+    view.isVisible = visible
+}
