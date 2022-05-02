d1622ab2e07bf9652ea3001ca11f3153e9f29929
hesamkotlin <hesam.naderi.kotlin@gmail.com>
2022-05-02 18:03:52 +0430

    add: Refactor mainActivity

--- app/src/main/java/com/example/interviewtestapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/interviewtestapp/ui/MainActivity.kt
@@ -8,15 +8,24 @@ import com.example.interviewtestapp.R
 import com.example.interviewtestapp.domain.UserRepository
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var userRepository: UserRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
+
+
 }
+
+
+
